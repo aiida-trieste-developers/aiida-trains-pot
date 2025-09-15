69386259438659d258ae08cd0ec51bd2bfbf5f49
Manko <manko.nm@gmail.com>
2025-09-15 14:28:07 +0200

    Build docs for GitHub Pages

--- src/aiida_trains_pot/portable_codes/mace_preprocess/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 mace_preprocess.py
--- src/aiida_trains_pot/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import restart
-from .install_portable_codes import install_committee_evaluation
-from .tools import center, enlarge_vacuum, error_calibration
-
-__all__ = ("restart","install_committee_evaluation", "center", "enlarge_vacuum", "error_calibration")
